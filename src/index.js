8eb9f4f1521bc0427105481711ac081faa0c070b
TribhanginD <tribhangin@gmail.com>
2025-04-16 12:26:43 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import mainRoutes from "./routes/main.js";
 import authRoutes from "./routes/auth.js";
 import emailRoutes from "./routes/email.js";
 
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+
 
 app.use("/main", mainRoutes);
 app.use("/auth", authRoutes);
